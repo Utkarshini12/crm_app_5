905ad81d4c5b7cb028f90a7aba05041fa29bcae2
Utkarshini <utkarshini@Utkarshinis-MacBook-Pro-2.local>
2022-10-08 14:58:33 +0530

    material table

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,7 +1,24 @@
+import MaterialTable from '@material-table/core';
+
+
+const lookup = { true: "Available", false: "Unavailable" };
+
+const columns = [
+  { title: "First Name", field: "firstName" },
+  { title: "Last Name", field: "lastName" },
+  { title: "Birth Year", field: "birthYear", type: "numeric" },
+  { title: "Availablity", field: "availability", lookup }
+];
+
+const data = [
+  { firstName: "Tod", lastName: "Miles", birthYear: 1987, availability: true },
+  { firstName: "Jess", lastName: "Smith", birthYear: 2000, availability: false }
+];
+
 function Admin() {
     return (
-        <div className="bg-primary vh-100">
-            Admin Page 
+        <div className="bg-primary vh-100  p-5">
+           <MaterialTable title="Demo Title" columns={columns} data={data} />
         </div>
     )
 }
